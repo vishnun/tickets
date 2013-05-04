87bdedfcd662c747f30adf6c064a980519cd5818
vishnun <vishnun@thoughtworks.com>
2013-05-04 13:38:22 +0530

    First commit - Basic scaffolding done for a ticket

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Tickets::Application.config.secret_token = 'f60fe752437c626fe0cddcd0153fc464098d006f242d86e11ce6e55ab0042305dea103956071b19c2d2cbd619755db21d738d24e4395ec8bf1f6364302f2117c'
